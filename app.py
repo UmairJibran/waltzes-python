ac070d5ff40cbc3160738863cffa10e7f4936712
Umair Jibran <me@umairjibran.com>
2024-11-08 21:12:23 +0500

    32-support-lever-board (#35)
    * ✨ feat: Copy to clipboard by button
    
    * ✨ feat: add support for python scrapping
    
    * ✨ feat: allow requesting from lever sites

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from dotenv import load_dotenv
 from flask_cors import CORS
 
-from main import fetch_job_details_from_greenhouse
+from main import fetch_job_details_from_greenhouse, fetch_job_details_from_lever
 from services.openai import generate_cover_letter
 from services.resume_best_match import get_best_match_from_resume
 from services.resume_vectorizor import vectorize_resume
@@ -33,6 +33,8 @@ def get_job_details(job_board):
     match job_board:
         case "greenhouse":
             job_details = fetch_job_details_from_greenhouse(job_url)
+        case "lever":
+            job_details = fetch_job_details_from_lever(job_url)
 
     resume_vectors, resume_segments = vectorize_resume()
     best_match_section = get_best_match_from_resume(job_details, resume_vectors, resume_segments)
--- main.py
+++ main.py
@@ -13,3 +13,16 @@ def fetch_job_details_from_greenhouse(url):
     text = text[:text.find("Apply for this job")]
 
     return text
+
+
+def fetch_job_details_from_lever(url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+    }
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    text = soup.get_text()
+    text = text[:text.find("Apply for this job")]
+
+    return text
